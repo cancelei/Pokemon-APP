3230ac8bf9d2a7a5a3169e5667d318cb6292ba71
Glauber <glauber.bannwart@gmail.com>
2023-07-04 17:12:25 -0300

    Updated Index.js and Create Helper JS file.

---
+++ src/cardUtils.js
@@ -0,0 +1,54 @@
+export function createCard() {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const imageElement = document.createElement('img');
+  imageElement.className = 'pokeimg';
+  imageElement.id = 'pokeimg';
+  imageElement.alt = 'pokeimage';
+  card.appendChild(imageElement);
+
+  const cardInfo = document.createElement('div');
+  cardInfo.className = 'card-info';
+  card.appendChild(cardInfo);
+
+  const titleParent = document.createElement('div');
+  titleParent.className = 'cardtitleparent';
+  cardInfo.appendChild(titleParent);
+
+  const titleElement = document.createElement('h2');
+  titleElement.className = 'cardtitle';
+  titleParent.appendChild(titleElement);
+
+  const likeButton = document.createElement('p');
+  likeButton.className = 'likebutton';
+  titleParent.appendChild(likeButton);
+
+  const likeButtonCounter = document.createElement('p');
+  likeButtonCounter.className = 'likebuttoncounter';
+  likeButtonCounter.textContent = '(Like Counter)';
+  cardInfo.appendChild(likeButtonCounter);
+
+  const commentButton = document.createElement('button');
+  commentButton.className = 'commentbutton btn';
+  commentButton.textContent = 'Comment';
+  cardInfo.appendChild(commentButton);
+
+  const reservationButton = document.createElement('button');
+  reservationButton.className = 'reservationbutton btn';
+  reservationButton.textContent = 'Reservation';
+  cardInfo.appendChild(reservationButton);
+
+  return card;
+}
+
+export function populateCard(card, data) {
+  const nameElement = card.querySelector('.cardtitle');
+  const imageElement = card.querySelector('.pokeimg');
+
+  const capitalizedFirstLetter = data.name.charAt(0).toUpperCase();
+  const modifiedName = capitalizedFirstLetter + data.name.slice(1);
+
+  nameElement.textContent = modifiedName;
+  imageElement.src = data.sprites.front_default;
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 import './style.css';
+import { createCard, populateCard } from './cardUtils.js';
 
-const cards = document.getElementsByClassName('card');
+const main = document.querySelector('.main');
 const fetchPromises = [];
 
-function populateCard(card, data) {
-  const nameElement = card.querySelector('.cardtitle');
-  const imageElement = card.querySelector('.pokeimg');
-
-  const capitalizedFirstLetter = data.name.charAt(0).toUpperCase();
-  const modifiedName = capitalizedFirstLetter + data.name.slice(1);
-
-  nameElement.textContent = modifiedName;
-  imageElement.src = data.sprites.front_default;
-}
-
-for (let i = 1; i <= cards.length; i + 1) {
-  const card = cards[i - 1];
+for (let i = 1; i <= 6; i += 1) {
+  const newCard = createCard();
+  main.appendChild(newCard);
 
   const fetchPromise = fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
     .then((response) => response.json())
     .then((data) => {
-      populateCard(card, data);
-    })
-    .catch(() => {});
+      populateCard(newCard, data);
+    });
 
   fetchPromises.push(fetchPromise);
 }
 
 Promise.all(fetchPromises)
   .then(() => {});
+
+// function populateCard(card, data) {
+//   const nameElement = card.querySelector('.cardtitle');
+//   const imageElement = card.querySelector('.pokeimg');
+
+//   const capitalizedFirstLetter = data.name.charAt(0).toUpperCase();
+//   const modifiedName = capitalizedFirstLetter + data.name.slice(1);
+
+//   nameElement.textContent = modifiedName;
+//   imageElement.src = data.sprites.front_default;
+// }
