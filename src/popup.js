4fe8abeff0eba6a76188af58370a05c8aa57e83f
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-07-06 01:44:46 +0430

    Fix eslint error

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,34 @@
+// ----- get Comments from api -----
+export function diplayComments() {
+  console.log('displayComments');
+  const form = document.querySelector('.form');
+  const itemId = form.id;
+  const commentList = document.querySelector('.comment-list');
+
+  const involvementApiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eT8XH25HH0nbRLIKTkDi';
+  console.log(itemId);
+  fetch(`${involvementApiUrl}/comments?item_id=${itemId}`)
+    .then((response) => (response.json()))
+    .then((data) => {
+      data.forEach((element) => {
+        const userComment = document.createElement('p');
+        userComment.innerHTML = `${element.creation_date}  ${element.username}: ${element.comment}`;
+        commentList.appendChild(userComment);
+      });
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 // -------- function to set the Details of each card popup --------
 function setPopupDetails(data) {
-  const img = document.querySelector(".pakanon-img");
-  const title = document.querySelector(".title");
-  const type = document.querySelector(".type");
-  const weight = document.querySelector(".weight");
-  const Length = document.querySelector(".length");
+  const img = document.querySelector('.pakanon-img');
+  const title = document.querySelector('.title');
+  const type = document.querySelector('.type');
+  const weight = document.querySelector('.weight');
+  const Length = document.querySelector('.length');
   const cardTitle = data.name.charAt(0).toUpperCase();
   const cardTitleUpperCase = cardTitle + data.name.slice(1);
   type.textContent = data.types[0].type.name;
@@ -17,8 +41,8 @@ function setPopupDetails(data) {
 
 // function to get data with an specific id
 export default function diplayCard(index) {
-  const form = document.querySelector(".form");
-  form.setAttribute("id", `${index}`);
+  const form = document.querySelector('.form');
+  form.setAttribute('id', `${index}`);
   const id = index;
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   fetch(url)
@@ -32,7 +56,7 @@ export default function diplayCard(index) {
 }
 
 // -- Create PopUp ----
-const Popup = document.querySelector(".Popup");
+const Popup = document.querySelector('.Popup');
 Popup.innerHTML = `
   <!-- The Modal -->
   <div id="myModal" class="modal">
@@ -77,49 +101,23 @@ Popup.innerHTML = `
   `;
 
 // Get the modal
-const modal = document.querySelector("#myModal");
+const modal = document.querySelector('#myModal');
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  modal.style.display = 'none';
   const commentList = document.querySelector('.comment-list');
-commentList.innerHTML='';
+  commentList.innerHTML = '';
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target === modal) {
-    modal.style.display = "none";
-  const commentList = document.querySelector('.comment-list');
-  commentList.innerHTML='';
+    modal.style.display = 'none';
+    const commentList = document.querySelector('.comment-list');
+    commentList.innerHTML = '';
   }
 };
-
-// ----- get Comments from api -----
-export  function diplayComments() {
-  console.log('displayComments');
-  const form = document.querySelector(".form");
-  const itemId = form.id;
-  const commentList = document.querySelector('.comment-list');
-   
-const involvementApiUrl = "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/eT8XH25HH0nbRLIKTkDi";
-console.log(itemId)
-  fetch(`${involvementApiUrl}/comments?item_id=${itemId}`)
-    .then((response) =>(response.json()))
-    .then((data) => {
-      data.forEach(element => {
-        const userComment = document.createElement('p');
-        userComment.innerHTML = `${element.creation_date}  ${element.username}: ${element.comment}`;
-        commentList.appendChild(userComment);
-      });
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
-
-
