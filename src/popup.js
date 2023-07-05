0a68e66ecf92f4c41553dd8765353859f1b801ea
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-07-05 14:13:45 +0430

    Fetch data from api and set the details for each cards in Popup

--- src/popup.js
+++ src/popup.js
@@ -1,56 +1,79 @@
-// function to get data with an specific id 
+// function to get data with an specific id
+let cardDetails = null; 
 export default function diplayCard(index) {
     const id = index;
-    const fetchPromise = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-      
+    const url=`https://pokeapi.co/api/v2/pokemon/${id}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setPopupDetails(data);
+    })
+    .catch(error => {
+      console.log('could not recive data')
     });
+  
 }
-const Popup = document.querySelector('.Popup');
-Popup.innerHTML = `
-<!-- The Modal -->
-<div id="myModal" class="modal">
+// -------- function to set the Details of each card popup --------
+function setPopupDetails(data){
+  const img = document.querySelector('.pakanon-img');
+  const title = document.querySelector('.title');
+  const type = document.querySelector('.type');
+  const weight = document.querySelector('.weight');
+  const Length = document.querySelector('.length')
+  type.textContent = data.types[0].type.name;
+  weight.textContent = data.weight;
+  Length.textContent = data.stats.length;
+  title.innerHTML =  data.species.name;
+  img.src = data.sprites.front_default;
+}
 
-  <!-- Modal content -->
-  <div class="modal-content">
-    <div class="modal-header">
-      <span class="close">&times;</span>
-      <img src="download.jpeg" alt="" class="pakanon-img">
+// -- Create PopUp ----
+  const Popup = document.querySelector('.Popup');
+  Popup.innerHTML = `
+  <!-- The Modal -->
+  <div id="myModal" class="modal">
+  
+    <!-- Modal content -->
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="close">&times;</span>
+        <img src="" alt="" class="pakanon-img">
+      </div>
+      <div class="modal-body">
+        <div class="modal-item">
+          <h2 class='title'>Space 3</h2>
+        </div>
+        <div class="modal-item">
+          <p>Type: <span class="type"></span></p>
+          <p>Length: <span class="length"></span></p>
+        </div>
+        <div class="modal-item">        
+          <p>Weight: <span class="weight"></span></p>
+        </div>
+        <div class="modal-item">
+          <h2>Comments (2)</h2>
+        </div>
+        <div class="modal-item">
+          <p class="comments"><span>2000/04/09</span> <B>Alex</B>: <i>Alex's comments</i></p>
+        </div>
+      </div>
+      <div >
+        <form action="" >
+          <fieldset class="add-cooment-form">
+            <legend>Add Comment</legend>
+            <input type="text" placeholder="Your Name">
+            <textarea name="" id="" cols="5" rows="5" placeholder="Your insight"></textarea>
+            <input type="submit" value="Comment" class="btn-comment">
+          </fieldset>
+        </form>
+      </div>
     </div>
-    <div class="modal-body">
-      <div class="modal-item">
-        <h2>Space 3</h2>
-      </div>
-      <div class="modal-item">
-        <p>Fuel: titanium</p>
-        <p>Length: 1000000</p>
-      </div>
-      <div class="modal-item">        
-        <p>Power: 100,000,000</p>
-        <p>Weight: 400</p>
-      </div>
-      <div class="modal-item">
-        <h2>Comments (2)</h2>
-      </div>
-      <div class="modal-item">
-        <p class="comments"><span>2000/04/09</span> <B>Alex</B>: <i>Alex's comments</i></p>
-      </div>
-    </div>
-    <div >
-      <form action="" >
-        <fieldset class="add-cooment-form">
-          <legend>Add Comment</legend>
-          <input type="text" placeholder="Your Name">
-          <textarea name="" id="" cols="5" rows="5" placeholder="Your insight"></textarea>
-          <input type="submit" value="Comment" class="btn-comment">
-        </fieldset>
-      </form>
-    </div>
+  
   </div>
+  `;
 
-</div>
-`;
+
 // Get the modal
 let modal = document.querySelector("#myModal");
 
