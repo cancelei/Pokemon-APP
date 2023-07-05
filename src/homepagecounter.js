3e428e7191b41c80334ed1c9c2f6a855f334e960
Glauber <glauber.bannwart@gmail.com>
2023-07-04 21:10:02 -0300

    Home Page Counter JS

---
+++ src/homepagecounter.js
@@ -0,0 +1,6 @@
+export function updateitemscount() {
+const counter = document.body.querySelectorAll('.card');
+
+const navli = document.getElementById("counter");
+navli.innerHTML = "Spaceships ( " + counter.length + " )";
+};
