6007dd60ce341b746c443e43a8b518c8cdb41754
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-07-06 19:18:47 +0430

    All test are passing

--- src/commentCounter.test.js
+++ src/commentCounter.test.js
@@ -1,15 +1,26 @@
-const { countComments } = require('./commentCounter.js');
+// Import the function to be tested
+import countComments from './commentCounter';
 
 describe('countComments', () => {
-  test('it should update the comment counter with the provided count', () => {
+  // Test case 1: When commentCounter is an empty string
+  test('should set the comment list text content to be an empty string', () => {
     // Arrange
     document.body.innerHTML = '<div class="counter"></div>';
-    const commentCounter = 10;
-
+    const commentCounter = '';
     // Act
     countComments(commentCounter);
-
     // Assert
-    expect(document.querySelector('.counter').textContent).toBe('10');
+    expect(document.querySelector('.counter').textContent).toBe('');
+  });
+
+  // Test case 2: When commentCounter is a non-empty string
+  test('should set the comment list text content to be the same as commentCounter', () => {
+    // Arrange
+    document.body.innerHTML = '<div class="counter"></div>';
+    const commentCounter = '5';
+    // Act
+    countComments(commentCounter);
+    // Assert
+    expect(document.querySelector('.counter').textContent).toBe('5');
   });
 });
