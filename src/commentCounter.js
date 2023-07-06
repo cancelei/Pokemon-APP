267e6739c8b78d97b1209f283539f9355b2161c6
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-07-06 13:40:35 +0430

    Add Counters for the Comments

---
+++ src/commentCounter.js
@@ -0,0 +1,9 @@
+export function countComments(commentCounter) {
+    const commentList = document.querySelector('.counter');
+    if(commentCounter == ''){
+    commentList.textContent = commentCounter; 
+    }else{
+    commentList.textContent =commentCounter; 
+    }
+  }
+  
